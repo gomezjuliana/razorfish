ace3f66e9de14bd9748910c14b651faf67ba6c40
Jules <jgmz.jg@gmail.com>
2017-05-02 17:52:33 -0500

    work on setup

--- razorfish.js
+++ razorfish.js
@@ -16,7 +16,7 @@ fetchJSONFile('gallery_json.js', function(data){
 	console.log(data);
 });*/
 
-function loadJSON(callback) {
+/*function loadJSON(callback) {
 	let jsonResponse = new XMLHttpRequest();
 		jsonResponse.overrideMimeType("application/json");
 	jsonResponse.open('GET', 'gallery_json.json', true);
@@ -34,4 +34,95 @@ function init(){
 	});
 }
 
-init();
+init();*/
+
+/*function XHR(file, callback){
+	let xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+		if (xhr.readyState === 4 && xhr.status === 200){
+			callback(xhr.responseText);
+		}
+	}
+	xhr.open('GET', file, true);
+	xhr.send();
+}
+
+function print(response){
+	console.log(response);
+}
+
+XHR('gallery_json.json', print);*/
+
+let info = {
+  "album": {
+    "name": "Intel 2013 Conference Moments"
+  },
+  "photos": [
+    {
+      "id":"1",
+      "image":"assets/image_1.jpg",
+      "thumb_url":"assets/thumb_1.jpg",
+      "title":"Experimental Projects at Intel",
+      "date":"June 17, 2013",
+      "location":"Richmond, Virginia"
+    },
+    {
+      "id":"2",
+      "image":"assets/image_2.jpg",
+      "thumb_url":"assets/thumb_2.jpg",
+      "title":"The Keynote Address Surprised Many",
+      "date":"June 18, 2013",
+      "location":"Raleigh, North Carolina"
+    },
+    {
+      "id":"3",
+      "image":"assets/image_3.jpg",
+      "thumb_url":"assets/thumb_3.jpg",
+      "title":"Jonathan Mathers",
+      "date":"July 16, 2013",
+      "location":"Austin, Texas"
+    },
+    {
+      "id":"4",
+      "image":"assets/image_4.jpg",
+      "thumb_url":"assets/thumb_4.jpg",
+      "title":"Next Generation Chipsets",
+      "date":"August 1, 2013",
+      "location":"New York, New York"
+    },
+   {
+      "id":"5",
+      "image":"assets/image_5.jpg",
+      "thumb_url":"assets/thumb_5.jpg",
+      "title":"Operating Systems of the Future",
+      "date":"September 3, 2013",
+      "location":"Chicago, Illinois"
+    },
+    {
+      "id":"6",
+      "image":"assets/image_6.jpg",
+      "thumb_url":"assets/thumb_6.jpg",
+      "title":"Sponsors of Tomorrow Booth",
+      "date":"September 14, 2013",
+      "location":"San Francisco, California"
+    }
+  ]
+}
+
+function print(obj){
+	document.getElementById('photo').src = obj.photos[5].image;
+	document.querySelector('.gallery figure figcaption h1').textContent = obj.photos[5].title;
+	document.querySelector('.gallery figure figcaption p').textContent = 'This photo was taken in '+obj.photos[5].location+' on '+obj.photos[5].date;
+}
+
+window.onload = function goForward(){
+	document.getElementById('forward').onclick = function fun(){
+		console.log('clicked!');
+	}
+}
+
+function goBackward(){
+
+}
+
+print(info);
