bd496a7911456a846b52baa2f998c885d0a67c51
Jules <jgmz.jg@gmail.com>
2017-05-09 17:33:20 -0500

    continue work on functions

--- razorfish.js
+++ razorfish.js
@@ -1,36 +1,44 @@
-function XHR(file, callback){
+function XHR(file){
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function(){
         if(xhr.readyState === 4 && xhr.status === 200){
         	let info = JSON.parse(xhr.responseText);
-            callback(info);
+            setUp(info);
         }
     }
     xhr.open('GET', file, true);
     xhr.send();
 }
 
-XHR('http://localhost:8000/Documents/Code-2017/razorfish/data.json', setUp);
+XHR('http://localhost:8000/Documents/Code-2017/razorfish/data.json');
 
 let count = 0;
 
 function setUp(info){
 	change(info, count);
+	document.getElementById('forward').addEventListener('click', goForward);
+	document.getElementById('back').addEventListener('click', goBackward);
+	document.querySelector('.thumbnails').addEventListener('click', changePhoto);
 }
 
-document.getElementById('forward').addEventListener('click', goForward);
-document.getElementById('back').addEventListener('click', goBackward);
-document.querySelector('.thumbnails').addEventListener('click', changePhoto);
-
 function goForward(info) {
+	console.log('click!')
 	console.log(info)
 	count = count >= info.photos.length-1 ? 0 : count+1;
-	change(info, count);
+	return change(info, count);
 }
 
 function goBackward(info){
+	console.log(info);
 	count = count <= 0 ? info.photos.length-1 : count-1;
 	change(info, count);
+}
+
+function change(info, count){
+	console.log(info);
+	document.querySelector('.gallery-curr').src = info.photos[count].image;
+	document.querySelector('figure figcaption h2').textContent = info.photos[count].title;
+	document.querySelector('figure figcaption p').textContent = 'This photo was taken on '+info.photos[count].date+' in '+info.photos[count].location;
 }
 
 function changePhoto(e){
@@ -42,9 +50,3 @@ function changePhoto(e){
 	document.querySelector('figure figcaption p').textContent = 'This photo was taken on '+info.photos[count].date+' in '+info.photos[count].location;
 }
 
-function change(info, count){
-	document.querySelector('.gallery-curr').src = info.photos[count].image;
-	document.querySelector('figure figcaption h2').textContent = info.photos[count].title;
-	document.querySelector('figure figcaption p').textContent = 'This photo was taken on '+info.photos[count].date+' in '+info.photos[count].location;
-	document.getElementById('one').classList.add('meow');
-}
