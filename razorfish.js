7f297455f1c708ba34ee5c35b6d475936a72c9a2
Jules <jgmz.jg@gmail.com>
2017-05-29 18:32:48 -0500

    fix thumbnail navigation

--- razorfish.js
+++ razorfish.js
@@ -16,40 +16,31 @@ let count = 0;
 
 function setUp(info){
 	change(info, count);
-	document.getElementById('forward').addEventListener('click', goForward(info));
+	document.getElementById('forward').addEventListener('click', () => goForward(info));
 	document.getElementById('back').addEventListener('click', () => goBackward(info));
-	// document.querySelector('.thumbnails').addEventListener('click', changePhoto);
+	for (let x = 0; x < info.photos.length; x++){
+		document.getElementById(x).addEventListener('click', () => change(info, x));
+	}
 }
 
 function goForward(info) {
-	return function() {
-		document.getElementById([count]).classList.remove('highlight');
-		count = count >= info.photos.length-1 ? 0 : count+1;
-		change(info, count);
-	}
+	count >= info.photos.length-1 ? change(info, 0) : change(info, count+1);
 }
 
 function goBackward(info){
-	document.getElementById([count]).classList.remove('highlight');
-	count = count <= 0 ? info.photos.length-1 : count-1;
-	change(info, count);
+	count <= 0 ? change(info, info.photos.length-1) : change(info, count-1);
 }
 
-function change(info, count){
-	document.querySelector('.gallery-curr').src = info.photos[count].image;
-	document.querySelector('figure figcaption h2').textContent = info.photos[count].title;
-	document.querySelector('figure figcaption p').textContent = 'This photo was taken on '+info.photos[count].date+' in '+info.photos[count].location;
-	for (let i = count; i < info.photos.length; i++){
+function change(info, count_){
+	for (let x = 0; x < info.photos.length; x++){
+		document.getElementById(x).classList.remove('highlight');
+	}
+	document.querySelector('.gallery-curr').src = info.photos[count_].image;
+	document.querySelector('figure figcaption h2').textContent = info.photos[count_].title;
+	document.querySelector('figure figcaption p').textContent = 'This photo was taken on '+info.photos[count_].date+' in '+info.photos[count_].location;
+	for (let i = count_; i < info.photos.length; i++){
 		document.getElementById([i]).src = info.photos[i].thumb_url;
 	}
-	document.getElementById([count]).classList.add('highlight');
-}
-
-function changePhoto(e){
-	console.log(e);
-	let targetElement = e.target || e.srcElement;
-	console.log(e.path[0]);
-	document.querySelector('.gallery-curr').src = targetElement.getAttribute('src');
-	document.querySelector('figure figcaption h2').textContent = info.photos[count].title;
-	document.querySelector('figure figcaption p').textContent = 'This photo was taken on '+info.photos[count].date+' in '+info.photos[count].location;
+	document.getElementById([count_]).classList.add('highlight');
+	count = count_
 }
